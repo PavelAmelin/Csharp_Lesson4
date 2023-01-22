00180ac77dd74a18391619d9924af56bc19287cb
Pavel <sivec_p@mail.ru>
2023-01-22 17:10:36 +0300

    Add Task29

---
+++ Task29/Program.cs
@@ -0,0 +1,19 @@
+﻿int[] Array()
+{
+    int N = Convert.ToInt32(Console.ReadLine());
+    int count = Convert.ToInt32(Console.ReadLine());
+    int [] arr = new int[count];
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(1, N);
+        if (i < arr.Length - 1)
+        Console.Write($"{arr[i]}, ");
+        else
+        Console.Write($"{arr[i]}");
+    }
+    Console.Write("]");
+    return arr;
+}
+
+Array();
