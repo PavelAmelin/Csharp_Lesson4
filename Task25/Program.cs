33d4b260eb52f34d4548c6229d6fe9ef0d46ef67
Pavel <sivec_p@mail.ru>
2023-01-22 16:59:20 +0300

    Add Task25

---
+++ Task25/Program.cs
@@ -0,0 +1,12 @@
+﻿int ToThePower(int A, int B)
+{
+    int res = A;
+    for (int i = 0; i < B - 1; i++) 
+    {
+        res *= A;
+    }
+    return res;
+}
+int x = Convert.ToInt32(Console.ReadLine());
+int y = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(ToThePower(x, y));
