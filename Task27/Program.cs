46a321908d74a741c17dc2f2c90e83448795b2d0
Pavel <sivec_p@mail.ru>
2023-01-22 17:02:01 +0300

    Add Task27

---
+++ Task27/Program.cs
@@ -0,0 +1,13 @@
+﻿int SumOfDig(int N)
+{
+    int res = 0;
+    while (N != 0)
+    {
+        res += N % 10;
+        N /= 10;
+    }
+    return res;
+}
+int N = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(SumOfDig(N));
